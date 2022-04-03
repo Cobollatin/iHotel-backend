fb4a5b2ed4f67304536c22aa43fbb0442f03944c
Cobollatin <Cobollatin@hotmail.com>
2022-04-03 08:53:27 -0500

    setup

---
+++ src/main/java/com/impactit/ihotel/IHotelApplication.java
@@ -0,0 +1,13 @@
+package com.impactit.ihotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IHotelApplication.class, args);
+    }
+
+}
